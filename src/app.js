edddbd132129846056ba4e2213a7f1ae5cd259a3
Gutclara <mariadelasclaras@gmail.com>
2021-04-22 13:05:44 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var figuras = ["♦", "♥", "♠", "♣"];
+  var numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  var indiceFigura = Math.floor(Math.random() * figuras.length);
+  var indiceNumero = Math.floor(Math.random() * numeros.length);
+
+  let figura = document.querySelector(".figura");
+  figura.innerHTML = `<h1>${figuras[indiceFigura]}</h1>`;
+
+  let figuraInv = document.querySelector(".invertida");
+  figuraInv.innerHTML = `<h1>${figuras[indiceFigura]}</h1>`;
+
+  let numero = document.querySelector(".numero");
+  numero.innerHTML = `<h1>${numeros[indiceNumero]}</h1>`;
+
+  if (indiceFigura <= 1) {
+    figura.style.color = "red";
+    figuraInv.style.color = "red";
+    numero.style.color = "red";
+  }
 };
